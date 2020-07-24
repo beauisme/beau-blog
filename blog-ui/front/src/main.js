f6c5b140e5d9a9328842e65ccace62b52af38d10
vbiao <1454248212@qq.com>
2020-07-24 13:27:54 +0800

    引入elementui

--- blog-ui/front/src/main.js
+++ blog-ui/front/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 
+// 设置项目中所有拥有size属性的组件默认尺寸为'small'，弹框的初始z-index为 3000
+Vue.use(Element,{ size: 'small', zIndex: 3000 });
+
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
